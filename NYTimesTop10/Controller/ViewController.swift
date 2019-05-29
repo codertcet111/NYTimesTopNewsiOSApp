b9c3fb32fbd3987d7789f3a1928dd63b57076a6b
Shubham Mishra <shubham.mishra@mirraw.com>
2019-05-29 10:36:25 +0530

    last modifications

--- NYTimesTop10/Controller/ViewController.swift
+++ NYTimesTop10/Controller/ViewController.swift
@@ -12,37 +12,43 @@ import SDWebImage
 
 class ViewController: UIViewController {
 
+    //Have used the Alamofire for API's request and response handling
     var alamoFireManager = Alamofire.SessionManager.default
     var model: TopNewsResults?
+    var activityView: UIActivityIndicatorView?
     
     @IBOutlet weak var newsTableViewOutlet: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.newsTableViewOutlet.estimatedRowHeight = self.newsTableViewOutlet.rowHeight
-        self.newsTableViewOutlet.rowHeight = UITableView.automaticDimension
+        self.newsTableViewOutlet.separatorColor = UIColor.black
+        //BuildDataSource will call the NYT's server and will set the table view
         self.buildDataSource()
         self.newsTableViewOutlet.estimatedRowHeight = self.newsTableViewOutlet.rowHeight
         self.newsTableViewOutlet.rowHeight = UITableView.automaticDimension
     }
     
     func buildDataSource(){
+        self.showActivityIndicator()
         self.alamoFireManager = getManagerWithConf()
+        //Below requesting to NYT's for section: home
         self.alamoFireManager.request(getRequestUrl("home"))
             .responseJSON { response in
+                self.stopActivityIndicator()
                 if( response.error != nil)
                 {
+                    self.showAlert(somethingWentWrongMessage)
                     return
                 }
                 guard response.result.error == nil else {
                     
                     if( response.result.error?._code == -999 )
                     {
+                        self.showAlert(somethingWentWrongMessage)
                         return
                     }
                     
-                    let alert = UIAlertController(title: noInternetMessage , message: nil , preferredStyle: UIAlertController.Style.alert)
-                    self.present(alert, animated: true, completion:{   })
+                    self.showAlert(noInternetMessage)
                     return
                 }
                 
@@ -73,7 +79,19 @@ class ViewController: UIViewController {
         }))
         self.present(alert, animated: true, completion: nil)
     }
-
+    
+    func showActivityIndicator(){
+        self.activityView = UIActivityIndicatorView(style: .whiteLarge)
+        activityView?.center = self.view.center
+        activityView?.hidesWhenStopped = true
+        activityView?.startAnimating()
+        activityView?.color = UIColor.black
+        self.view.addSubview(activityView!)
+    }
+    
+    func stopActivityIndicator(){
+        self.activityView?.stopAnimating()
+    }
 
 }
 
@@ -91,25 +109,24 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource{
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! NewsTableViewCell
         if let tempNews = self.model?.results[indexPath.row]{
             cell.newsTitle.text = tempNews.title
-//            cell.newsTitle.sizeToFit()
             if tempNews.multimedia.count >= 4{
                 let imageURL = URL(string: tempNews.multimedia[3].url)!
                 cell.newsImage.sd_setShowActivityIndicatorView(true)
                 cell.newsImage.sd_setIndicatorStyle(.gray)
                 cell.newsImage.contentMode = UIView.ContentMode.scaleAspectFit
-                //            cell.newsImage.sd_setImage(imageURL, placeholderImage:UIImage(imageNamed:"NYTimesPlaceholder.jpeg"))
                 cell.newsImage.sd_setImage(with: imageURL, placeholderImage: UIImage(named:"NYTimesPlaceholder.jpeg"))
             }
             cell.newsDescription.text = tempNews.abstract
-//            cell.newsDescription.sizeToFit()
             
         }
         return cell
     }
     
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 0.0
-//    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let url = self.model?.results[indexPath.row].url, #available(iOS 10, *){
+            UIApplication.shared.open(URL(string: url)!, options: [:])
+        }
+    }
     
 }
 
